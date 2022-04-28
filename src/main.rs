166d90b9f12d20b2c3c2d1984e1e64414fde7c20
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-28 15:07:03 +0200

    forward verbatim input from socket

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use std::{io, path::PathBuf, process::Stdio, sync::atomic::AtomicU64};
+use std::{
+    error::Error,
+    io,
+    net::SocketAddr,
+    path::PathBuf,
+    process::Stdio,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+};
 
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 use clap::Parser;
 use tokio::{
+    io::{AsyncWriteExt, BufWriter},
     process::{ChildStdin, ChildStdout, Command},
     sync::{mpsc, Mutex},
 };
@@ -9,6 +26,8 @@ use tokio::{
 #[derive(Debug, Parser)]
 struct Opt {
     engine: PathBuf,
+    #[clap(long, default_value = "127.0.0.1:9670")]
+    bind: SocketAddr,
 }
 
 fn secret() -> String {
@@ -16,12 +35,12 @@ fn secret() -> String {
 }
 
 struct Engine {
-    current: AtomicU64,
+    current_handler: AtomicU64,
     pipes: Mutex<EnginePipes>,
 }
 
 struct EnginePipes {
-    stdin: ChildStdin,
+    stdin: BufWriter<ChildStdin>,
     stdout: ChildStdout,
 }
 
@@ -31,12 +50,13 @@ impl Engine {
             .stdout(Stdio::piped())
             .stdin(Stdio::piped())
             .spawn()?;
+
         Ok(Engine {
-            current: AtomicU64::new(0),
+            current_handler: AtomicU64::new(0),
             pipes: Mutex::new(EnginePipes {
-                stdin: process.stdin.take().ok_or_else(|| {
+                stdin: BufWriter::new(process.stdin.take().ok_or_else(|| {
                     io::Error::new(io::ErrorKind::BrokenPipe, "engine stdin closed")
-                })?,
+                })?),
                 stdout: process.stdout.take().ok_or_else(|| {
                     io::Error::new(io::ErrorKind::BrokenPipe, "engine stdout closed")
                 })?,
@@ -46,8 +66,54 @@ impl Engine {
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::parse();
-    let engine = Engine::new(opt.engine).await?;
+
+    let engine = Arc::new(Engine::new(opt.engine).await?);
+
+    let app = Router::new().route(
+        "/:secret",
+        get({
+            let engine = Arc::clone(&engine);
+            move |ws| handler(engine, ws)
+        }),
+    );
+
+    axum::Server::bind(&opt.bind)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+async fn handler(engine: Arc<Engine>, ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_socket(engine, socket))
+}
+
+async fn handle_socket(engine: Arc<Engine>, socket: WebSocket) {
+    handle_socket_inner(engine, socket).await;
+}
+
+async fn handle_socket_inner(
+    engine: Arc<Engine>,
+    mut socket: WebSocket,
+) -> Result<(), axum::Error> {
+    let current_handler = engine.current_handler.fetch_add(1, Ordering::SeqCst) + 1;
+    let mut pipes = engine.pipes.lock().await;
+    while current_handler == engine.current_handler.load(Ordering::SeqCst) {
+        tokio::select! {
+            msg = socket.recv() => {
+                match msg {
+                    Some(Ok(Message::Text(text))) => pipes.stdin.write_all(text.as_bytes()).await.map_err(|err| axum::Error::new(err))?,
+                    Some(Ok(Message::Pong(_))) => (),
+                    Some(Ok(Message::Ping(data))) => socket.send(Message::Pong(data)).await?,
+                    Some(Ok(Message::Binary(binary))) => pipes.stdin.write_all(&binary).await.map_err(|err| axum::Error::new(err))?,
+                    None | Some(Ok(Message::Close(_))) => break,
+                    Some(Err(err)) => return Err(err),
+                }
+            }
+        }
+    }
+    socket.send(Message::Close(None)).await?;
     Ok(())
 }
