f2d604cde9396bab4f28774ff320bab91fcb67f8
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-08-05 21:27:05 +0200

    actually do sth with engineconfig

--- libexternalengine/src/lib.rs
+++ libexternalengine/src/lib.rs
@@ -1,14 +1,15 @@
 #[repr(C)]
 struct EngineConfig {
-    auth_token: *const u8,
-    engine_path: *const u8,
+    auth_token: *const i8,
+    engine_path: *const i8,
     max_hash: i32,
     max_threads: i32,
 }
 
 #[no_mangle]
 fn StartListening(config: *const EngineConfig) -> i32 {
-    1
+    let s = unsafe {std::ffi::CStr::from_ptr((*config).auth_token) };
+    s.to_str().expect("utf-8").parse().expect("int")
 }
 
 #[no_mangle]
