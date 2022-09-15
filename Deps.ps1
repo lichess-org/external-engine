ab77cd200b5b1e265cd8e38a3f2ade4f99976735
jonbbbb <jrbrock+jonbbbb@gmail.com>
2022-09-15 11:40:27 -0400

    Fix #18 by moving pipes to end of line instead of beginning

--- Deps.ps1
+++ Deps.ps1
@@ -2,8 +2,8 @@ git submodule update --init --recursive
 
 Set-Location .\stockfish\vendor
 
-Get-Content downloads.txt
-| Where-Object { -Not (Test-Path $(Split-Path $_ -Leaf)) }
-| ForEach-Object { Invoke-WebRequest $_ -OutFile $(Split-Path $_ -Leaf) }
+Get-Content downloads.txt |
+   Where-Object { -Not (Test-Path $(Split-Path $_ -Leaf)) } |
+   ForEach-Object { Invoke-WebRequest $_ -OutFile $(Split-Path $_ -Leaf) }
 
 Set-Location .\..\..
