deaa81483aa0a9a24c38b095a8425c92cb0b18bc
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-14 00:55:55 +0200

    delete outdated remote-uci

--- remote-uci-service/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-	println!("cargo:rerun-if-changed=Cargo.toml");
-	println!("cargo:rerun-if-changed=favicon.ico");
-	winres::WindowsResource::new()
-	    .set_icon("favicon.ico")
-		.compile()
-		.expect("winres");
-}
