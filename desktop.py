49970596a842d1ea9e97cbe05d2ccd123915358a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-23 12:14:49 +0200

    generate oauth url

--- desktop.py
+++ desktop.py
@@ -8,16 +8,39 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
+import secrets
 import sys
+import hashlib
 import http.server
+import urllib.parse
+import base64
+
+def code_challenge(code_verifier):
+    h = hashlib.sha256()
+    h.update(code_verifier.encode("ascii"))
+    return base64.urlsafe_b64encode(h.digest())
 
 class OAuthRequestHandler(http.server.BaseHTTPRequestHandler):
     pass
 
 class OAuthServer:
     def __init__(self):
-        self.server = http.server.HTTPServer(("", 8080), OAuthRequestHandler)
-        self.server.serve_forever()
+        self.code_verifier = secrets.token_urlsafe(32)
+        self.state = secrets.token_urlsafe(32)
+
+        self.server = http.server.HTTPServer(("127.0.0.1", 0), OAuthRequestHandler)
+        ip, port = self.server.server_address
+
+    def url(self):
+        params = urllib.parse.urlencode({
+            "response_type": "code",
+            "client_id": "com.github.lichess_org.external_engine",
+            "code_challenge_method": "S256",
+            "code_challenge": code_challenge(self.code_verifier),
+            "scope": "engine:read engine:write",
+            "state": self.state,
+        })
+        return f"https://lichess.org/oauth?{params}"
 
 
 class MainWindow(QMainWindow):
@@ -28,6 +51,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     server = OAuthServer()
+    print(server.url())
     sys.exit(0)
 
     app = QApplication(sys.argv)
